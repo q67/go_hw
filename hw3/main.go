95e83572ab2ed57e62043be2fbecb207d67ac480
Victor <victor.banudz@smartyads.com>
2024-03-16 06:41:47 +0200

    fixes by conversations

--- hw3/main.go
+++ hw3/main.go
@@ -4,35 +4,28 @@ import (
 	"fmt"
 )
 
-var haveCode bool = false
-
-func getKey(scenes map[sceneId]string) sceneId {
-	keys := make([]sceneId, len(scenes))
-	i := 0
-	for k := range scenes {
-		keys[i] = k
-		i++
-	}
-
-	return keys[0]
+type hero struct {
+	name     string
+	haveCode bool
 }
+
+var Hero hero
 
 func printScene(sceneDesc string) {
 	fmt.Print("\n> ", sceneDesc, "\n\n")
 }
 
-func getWays(chooses scenesList) {
+func printWays(chooses []nextScene) {
 	var str string
 	for i, choose := range chooses {
-		key := getKey(choose)
-		str += fmt.Sprint(i+1) + ") " + string(choose[key]) + "\n"
+		str += fmt.Sprint(i+1) + ") " + string(choose.action) + "\n"
 	}
 	str += " \n0) " + string(Exit) + "\n\n> "
 
 	fmt.Print(str)
 }
 
-func getSceneId(scanIndex int, scenes scenesList) sceneId {
+func getSceneId(scanIndex int, scenes []nextScene) sceneId {
 	index := scanIndex - 1
 	scenesLen := len(scenes)
 
@@ -44,42 +37,47 @@ func getSceneId(scanIndex int, scenes scenesList) sceneId {
 		return Exit
 	}
 
-	scene := getKey(scenes[index])
-
-	if scene == Flashlight {
-		haveCode = true
-	}
-
-	if scene == Start {
-		haveCode = false
-	}
+	scene := scenes[index].nextSceneId
 
 	return scene
 }
 
+func itemsModify(scene sceneId) {
+	if scene == Flashlight {
+		Hero.haveCode = true
+	}
+
+	if scene == Start {
+		Hero.haveCode = false
+	}
+}
+
 func updateScenes(scene sceneId, scenes scenesIn) scenesIn {
-	if haveCode && scene == Tent {
-		*scenes[Tent].nextScenes = append(*scenes[Tent].nextScenes, map[sceneId]string{StrongBoxOpen: "Ввести код зі стіни печери"})
-		haveCode = false
+	if Hero.haveCode && scene == Tent {
+		*scenes[Tent].nextScenes = append(*scenes[Tent].nextScenes, nextScene{StrongBoxOpen, "Ввести код зі стіни печери"})
+		Hero.haveCode = false
 	}
 
 	return scenes
 }
 
 func main() {
-	var scenes map[sceneId]scene = InitScenes()
-	var currSceneId sceneId = Start
+	Hero.name = "Стівен"
+	Hero.haveCode = false
+	scenes := InitScenes()
+	currSceneId := Start
 	var scanIndex int
 
 	for {
 		printScene(scenes[currSceneId].desc)
-		getWays(*scenes[currSceneId].nextScenes)
+		printWays(*scenes[currSceneId].nextScenes)
 		fmt.Scanf("%d", &scanIndex)
 		currSceneId = getSceneId(scanIndex, *scenes[currSceneId].nextScenes)
 		if currSceneId == Exit {
-			fmt.Println("Гру завершено.")
+			fmt.Printf("Гру завершено, бувай, %s.\n", Hero.name)
 			break
 		}
+		itemsModify(currSceneId)
 		scenes = updateScenes(currSceneId, scenes)
 	}
 }
